b9d6812071ca239da01bcf80cf6fc917984174cb
bernard <bernard.ballesta@neo9.fr>
2022-06-14 19:50:42 +0200

    mise en page

--- cout.py
+++ cout.py
@@ -1,17 +1,24 @@
-# Fonction de cout
+# Fonction de coût
 from array import array
 
 def f(entrees,parametres) -> array:
     return [0.8, 0.2, 0.1] 
 
-def Cout(entrees,sorties_desirees,parametres) -> float:
+# Cout
+def Cout(entrees : array,
+         sorties_desirees : array,
+         parametres : array) -> float:
+
     sorties_produites = f(entrees,parametres)
     cout = 0
     for j in range(len(sorties_desirees)):
         cout= cout+ (sorties_desirees[j]-sorties_produites[j])**2
     return cout    
 
-def Transcode(image) -> array:
+# Transcode 
+#   - de la représentation en caractère
+#   - vers du mumérique (0,1)
+def Transcode(image : array) -> array:
     lignes, colonnes = len(image), len(image[0])
     image_numerique = [[0]*lignes for _ in range(colonnes)]
     l = 0
@@ -27,18 +34,25 @@ def Transcode(image) -> array:
         l= l+1    
     return image_numerique;        
 
+# Affiche une image ligne à ligne
 def affiche_image(etiquette : str, 
                   image : array):
     print(etiquette)
     for ligne in image:
-        print(ligne)
+        print("    ", ligne)
     
-def affiche(texte : str) ->None:
-    print(texte)
+# affiche un texte sousligné    
+def affiche_ligne(texte : str) ->None:
+    ligne = "";
     for c in texte:
-        print ('=',end='')
-    print ('\n')   
+        ligne = ligne + "="
+    print ("")    
+    print (ligne)   
+    print(texte)
+    print(ligne)   
+    print ("")
 
+# Images lisibles ("+" ou "-")  
 c1= Transcode([
         #01234
         "  |  ", # 0
@@ -64,8 +78,8 @@ c3= Transcode([
         "----."
     ])
 
-affiche("Fonction de cout")
-print("Images à reconnaitre")
+affiche_ligne("Fonction de coût")
+print("Images à reconnaitre:")
 affiche_image('1', c1)
 affiche_image('2', c2)
 affiche_image('3', c3)
